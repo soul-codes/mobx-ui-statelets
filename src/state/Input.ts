0f75bcac0c37fb15c096538a00572b810dc49831
soul-codes <reach.soul.codes@gmail.com>
2018-08-22 10:11:08 +0200

    :sparkles: input choice query

--- src/state/Input.ts
+++ src/state/Input.ts
@@ -2,26 +2,34 @@ import { observable, action } from "mobx";
 import Validator from "./Validator";
 import State, { StateDevOptions } from "./State";
 import Form from "./Form";
-import { Falsy } from "../utils/types";
+import { Falsy, MaybeConstant, MaybePromise } from "../utils/types";
 import withHover from "../partials/withHover";
+import Actuator from "./Actuator";
 
 let confirmCounter = 0;
 
-export default class Input<TValue extends BaseInputValue> extends withHover(
-  State
-) {
+export default class Input<
+  TValue extends BaseInputValue,
+  TChoiceEvaluation = never
+> extends withHover(State) {
   constructor(
     readonly defaultValue: TValue,
-    readonly options?: InputOptions<TValue>
+    readonly options?: InputOptions<TValue, TChoiceEvaluation>
   ) {
     super(options);
     this._value = defaultValue === void 0 ? ("" as TValue) : defaultValue;
+
+    const choices = options && options.choices;
+    this._choiceActuator = choices
+      ? new Actuator(typeof choices === "function" ? choices : () => choices)
+      : null;
   }
 
   @action
   input(value: TValue) {
     if (this.isBeingSubmitted) return;
     this._inputValue = value;
+    this.queryChoices();
   }
 
   normalizeValue(value: TValue) {
@@ -76,6 +84,44 @@ export default class Input<TValue extends BaseInputValue> extends withHover(
   }
 
   @action
+  async queryChoices() {
+    const choiceActuator = this._choiceActuator;
+    if (!choiceActuator) return;
+
+    const { options, inputValue } = this;
+    const choiceQueryLimit = options && options.choiceQueryLimit;
+    this._lastQuery = inputValue;
+    await choiceActuator.invoke({
+      inputValue: this.inputValue,
+      limit: choiceQueryLimit === void 0 ? Infinity : choiceQueryLimit,
+      offset: 0
+    });
+    this._choices = choiceActuator.result ? choiceActuator.result.choices : [];
+  }
+
+  @action
+  async queryMoreChoices() {
+    const choiceActuator = this._choiceActuator;
+    if (!choiceActuator) return;
+    if (choiceActuator.isPending)
+      return choiceActuator.promise as Promise<void>;
+    if (!choiceActuator.result) return this.queryChoices();
+
+    const { inputValue } = this;
+    if (inputValue !== this._lastQuery) return this.queryChoices();
+    if (choiceActuator.result.stats.isDone) return;
+
+    const { options } = this;
+    const choiceQueryLimit = options && options.choiceQueryLimit;
+    await choiceActuator.invoke({
+      inputValue: this.inputValue,
+      limit: choiceQueryLimit === void 0 ? Infinity : choiceQueryLimit,
+      offset: this._choices.length
+    });
+    this._choices.push(...choiceActuator.result.choices);
+  }
+
+  @action
   async validate() {
     const confirmId = this._confirmId;
     const set = new Set<Validator<any, any, any>>();
@@ -104,6 +150,25 @@ export default class Input<TValue extends BaseInputValue> extends withHover(
 
   get value() {
     return this._value;
+  }
+
+  get hasMoreChoices() {
+    const choiceActuator = this._choiceActuator;
+    if (!choiceActuator) return false;
+    if (!choiceActuator.result) return true;
+    return !choiceActuator.result.stats.isDone;
+  }
+
+  get totalChoices() {
+    const choiceActuator = this._choiceActuator;
+    if (!choiceActuator) return 0;
+    if (!choiceActuator.result) return null;
+    const total = choiceActuator.result.stats.total;
+    return total === void 0 ? null : total;
+  }
+
+  get isQueryingChoices() {
+    return Boolean(this._choiceActuator && this._choiceActuator.isPending);
   }
 
   get isValidated() {
@@ -154,18 +219,58 @@ export default class Input<TValue extends BaseInputValue> extends withHover(
   __$$private_forms = new Set<Form<any, any>>();
 
   private _confirmId = 0;
+
+  private _choiceActuator: Actuator<
+    InputChoiceQuery<TValue>,
+    InputChoiceQueryResult<TValue, TChoiceEvaluation>
+  > | null;
+
+  @observable.shallow
+  private _choices: InputChoice<TValue, TChoiceEvaluation>[] = [];
+  private _lastQuery: TValue | null = null;
 }
 
 function defaultShouldValidate<TValue>(value: TValue, oldValue: TValue) {
   return value !== oldValue;
 }
 
-export interface InputOptions<TValue> extends StateDevOptions {
+export interface InputOptions<
+  TValue extends BaseInputValue,
+  TChoiceEvaluation = never
+> extends StateDevOptions {
   readonly normalizer?: ((value: TValue) => TValue) | Falsy;
   revalidate?: (value: TValue, oldValue: TValue) => boolean;
+  choices?: MaybeConstant<
+    (
+      query: InputChoiceQuery<TValue>
+    ) => MaybePromise<InputChoiceQueryResult<TValue, TChoiceEvaluation>>
+  >;
+  choiceQueryLimit?: number;
 }
 
 export type BaseInputValue = string | number | boolean;
 export type InputValue<T extends Input<any>> = T extends Input<infer V>
   ? V
   : never;
+
+export interface InputChoice<TValue extends BaseInputValue, TChoiceEvaluation> {
+  value: TValue;
+  evaluation: TChoiceEvaluation;
+}
+
+export interface InputChoiceQuery<TValue extends BaseInputValue> {
+  inputValue: TValue;
+  offset: number;
+  limit: number;
+}
+
+export interface InputChoiceQueryResult<
+  TValue extends BaseInputValue,
+  TChoiceEvaluation
+> {
+  choices: InputChoice<TValue, TChoiceEvaluation>[];
+  stats: {
+    isDone?: boolean;
+    total?: number;
+  };
+}
