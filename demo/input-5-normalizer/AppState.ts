7399ad1ae5a1ec0a7094f3cb88bb58152d878af4
soul-codes <reach.soul.codes@gmail.com>
2018-10-04 10:57:29 +0200

    :fire: Remove currently unused demo

--- demo/input-5-normalizer/AppState.ts
+++
@@ -1,7 +0,0 @@
-import { Input } from "mobx-ui-statelets";
-
-export default class AppState {
-  myInputState = new Input<string>("", {
-    normalizer: value => value.trim().toLowerCase()
-  });
-}
